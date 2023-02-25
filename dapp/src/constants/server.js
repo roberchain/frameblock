f2d7ff50b7cc49d2a8d6722f0b8adcf1bbe188dd
roberchain <106393494+roberchain@users.noreply.github.com>
2023-02-25 07:43:23 +0100

    Delete dapp directory

--- dapp/src/constants/server.js
+++
@@ -1,5 +0,0 @@
-const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-  ? 'http://localhost:8081'
-  : 'http://localhost:8081';
-
-export default PAYMENT_SERVER_URL;
--- dapp/src/constants/stripe.js
+++
@@ -1,5 +0,0 @@
-const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_test_'
-  : 'pk_test_';
-
-export default STRIPE_PUBLISHABLE;
